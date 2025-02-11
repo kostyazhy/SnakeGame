64842db0d8909b4779335f4742e22d66cd63276f
Kostay_Zhu <konstantin.zhukov@ocrv.ru>
2025-02-11 18:03:37 +0300

    Add auto build project

---
+++ Build.bat
@@ -0,0 +1,6 @@
+"c:\Program Files\Epic Games\UE_5.1\Engine\Build\BatchFiles\RunUAT.bat" ^
+BuildCookRun ^
+-project="c:\work\MasterValera\MasterValera.uproject" ^
+-platform=Win64 -clientconfig=Development ^
+-archivedirectory="c:\work\MasterValera\BuildFromBat" ^
+-cook -stage -archive -pak -package -allmaps 
