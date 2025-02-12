4ea3ed7fcab9e0935718459a10541912ac32cabc
kostayzhy <konstantin.zhukov@ocrv.ru>
2025-02-12 16:17:05 +0300

    Change path project

--- Build.bat
+++ Build.bat
@@ -1,6 +1,6 @@
 "c:\Program Files\Epic Games\UE_5.1\Engine\Build\BatchFiles\RunUAT.bat" ^
 BuildCookRun ^
--project="c:\work\MasterValera\MasterValera.uproject" ^
+-project="c:\work\SnakeGame\SnakeGame.uproject" ^
 -platform=Win64 -clientconfig=Development ^
--archivedirectory="c:\work\MasterValera\BuildFromBat" ^
+-archivedirectory="c:\work\SnakeGame\BuildFromBat" ^
 -cook -stage -archive -pak -package -allmaps 
